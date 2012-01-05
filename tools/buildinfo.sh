f10c970ffe1b65807217eb4f1c169fba06087674
Robert Burns <burnsra@gmail.com>
2012-01-04 20:14:36 -0500

    Add ro.omega.device to build.prop
    As requested by Ricardo Cerqueira:
    http://r.cyanogenmod.com/#change,11423,patchset=1
    
    Change-Id: If08cc13c0f0e92e0dd262a2f71610070a54aaead
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -64,4 +64,6 @@ if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
+echo "ro.omega.device=$OMEGA_DEVICE"
+
 echo "# end build properties"
