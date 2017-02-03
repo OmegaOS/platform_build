a3bb2144eea0092215d49977d100c84933400132
karthick111 <karthickrko61@gmail.com>
2017-02-03 19:02:17 +0100

    roomservice: Track OmegaOS
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Copyright (C) 2012-2013, The CyanogenMod Project
 # Copyright (C) 2012-2015, SlimRoms Project
+# Copyright (C) 2017, OmegaOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,11 +43,11 @@ except ImportError:
 DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
-custom_local_manifest = ".repo/local_manifests/slim_manifest.xml"
-custom_default_revision = "ng7.0"
-custom_dependencies = "slim.dependencies"
-org_manifest = "SlimRoms"  # leave empty if org is provided in manifest
-org_display = "SlimRoms"  # needed for displaying
+custom_local_manifest = ".repo/local_manifests/omega_manifest.xml"
+custom_default_revision = "n7.1"
+custom_dependencies = "omega.dependencies"
+org_manifest = "OmegaOS"  # leave empty if org is provided in manifest
+org_display = "OmegaOS"  # needed for displaying
 
 github_auth = None
 
